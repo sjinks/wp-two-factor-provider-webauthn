8d32177e7f68b120ab00de0be60dc9fdff3eaa46
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2022-05-17 11:21:22 +0300

    Synchronize plugin version across all files

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /*
  * Plugin Name: WebAuthn Provider for Two Factor
  * Description: WebAuthn Provider for Two Factor plugin.
- * Version: 1.0.4
+ * Version: 1.0.5
  * Author: Volodymyr Kolesnykov
  * License: MIT
  * Text Domain: two-factor-provider-webauthn

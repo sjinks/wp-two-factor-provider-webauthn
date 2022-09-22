8500e86151d11fd64314aee1f8fe349f1e1f98df
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2022-09-22 10:21:32 +0300

    Update scripts

--- bin/build-archive.sh
+++ bin/build-archive.sh
@@ -3,8 +3,7 @@
 (
     cd .. && \
     git archive --format=tar --prefix=two-factor-provider-webauthn/ HEAD | (cd /var/tmp/ && tar xf -) && \
-    cp composer.lock /var/tmp/two-factor-provider-webauthn/ && \
-    (cd /var/tmp/two-factor-provider-webauthn && composer install --no-dev --no-interaction && composer remove --update-no-dev --no-interaction composer/installers && rm -f composer.lock) && \
+    (cd /var/tmp/two-factor-provider-webauthn && composer install --no-dev --no-interaction && composer remove --update-no-dev --no-interaction composer/installers cweagans/composer-patches && rm -rf composer.lock patches vendor/madwizard/webauthn/.github vendor/madwizard/webauthn/conformance vendor/madwizard/webauthn/tests vendor/psr/log/Psr/Log/Test) && \
     (cd /var/tmp && zip -r -9 two-factor-provider-webauthn.zip two-factor-provider-webauthn) && \
     mv /var/tmp/two-factor-provider-webauthn.zip two-factor-provider-webauthn.zip
 )

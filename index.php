58b37453307495eb9cfe2d34746a8400dc02a173
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2022-10-14 04:31:15 +0300

    Update changelog and bump version

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /*
  * Plugin Name: WebAuthn Provider for Two Factor
  * Description: WebAuthn Provider for Two Factor plugin.
- * Version: 1.0.10
+ * Version: 2.0.0
  * Author: Volodymyr Kolesnykov
  * License: MIT
  * Text Domain: two-factor-provider-webauthn

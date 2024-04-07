bcb69a785a3a2ebbaa225867bd5a937c811b322e
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2024-04-07 05:15:55 +0300

    ci: update scripts

--- bin/reset-database.sh
+++ bin/reset-database.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-set -x
-set -e
+set -ex
 
 cd "$(dirname "$0")/.."
 
-docker-compose exec -T wordpress rm -rf /var/www/html/wp-content/mu-plugins
-docker-compose run --rm cli sh -c '\
+docker compose exec -T wordpress rm -rf /var/www/html/wp-content/mu-plugins
+docker compose run --rm cli sh -c '\
     wp db reset --yes && \
     wp core install --url="https://localhost:8443" --title="Test Site" --admin_user=admin --admin_password=password --admin_email=wordpress@example.com --skip-email && \
     wp rewrite structure "/%postname%/" && \

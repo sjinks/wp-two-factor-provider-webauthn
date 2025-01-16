be37e9fc851440c07188b9f3fbb6e4c9267ce00c
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2025-01-16 07:58:19 +0200

    chore: bump plugin version

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /*
  * Plugin Name: WebAuthn Provider for Two Factor
  * Description: WebAuthn Provider for Two Factor plugin.
- * Version: 2.5.0
+ * Version: 2.5.1
  * Author: Volodymyr Kolesnykov
  * License: MIT
  * Text Domain: two-factor-provider-webauthn

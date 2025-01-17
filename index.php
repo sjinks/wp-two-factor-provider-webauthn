f7754853d75486b4fe7d65184007d6cb30c89aad
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2025-01-17 11:28:19 +0000

    chore: prepare 2.5.2 release

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /*
  * Plugin Name: WebAuthn Provider for Two Factor
  * Description: WebAuthn Provider for Two Factor plugin.
- * Version: 2.5.1
+ * Version: 2.5.2
  * Author: Volodymyr Kolesnykov
  * License: MIT
  * Text Domain: two-factor-provider-webauthn

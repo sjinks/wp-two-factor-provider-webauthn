4f924f6d38bb232bfc2d1ab6679c73f599146af3
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2026-02-23 16:12:15 +0200

    fix: backport changes from WPVIP Auth

--- inc/class-twofactor-provider-webauthn.php
+++ inc/class-twofactor-provider-webauthn.php
@@ -2,6 +2,9 @@
 
 use WildWolf\WordPress\TwoFactorWebAuthn\WebAuthn_Provider;
 
-final class TwoFactor_Provider_WebAuthn extends WebAuthn_Provider {
+/**
+ * @api
+ */
+class TwoFactor_Provider_WebAuthn extends WebAuthn_Provider {
 
 }
